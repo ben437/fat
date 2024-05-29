3f44c341a6da10385041b7b43a145dc15fdf2958
ben437 <82418249+ben437@users.noreply.github.com>
2024-05-28 22:17:03 -0700

    Update key.lua

--- key.lua
+++ key.lua
@@ -1,7 +1,8 @@
 local blacklist = {
 	["dieumanase123"] = true,
 	["fat"] = true,
-    ["benk20r"] = true
+	["benk20r"] = true
+    
 }
 
 local blacklistfind = blacklist[game.Players.LocalPlayer.Name]
@@ -97,9 +98,44 @@ CloseCallback = <function> - Function to execute when the window is closed.
 else
 	if blacklistfind == nil then
 print("Your safe, For now....")
-		print("sigma")
+		
  loadstring(game:HttpGet('https://raw.githubusercontent.com/ben437/fat/main/yeah.rizz'))()
 else
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Blacklisted", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Tab = Window:MakeTab({
+	Name = "Blacklisted",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+Tab:AddParagraph("Why were you blacklisted?","You left the discord after using key.")
+Tab:AddButton({
+	Name = "Click to copy the discord server link to be unblacklisted",
+	Callback = function()
+      	setclipboard("https://discord.gg/TzceVeAbf9")
+  	end    
+})
+Tab:AddParagraph("How to be unblacklisted?","Mention the server manager, If no response wait.")
+--[[
+Name = <string> - The name of the button.
+Callback = <function> - The function of the button.
+]]
+--[[
+Name = <string> - The name of the tab.
+Icon = <string> - The icon of the tab.
+PremiumOnly = <bool> - Makes the tab accessible to Sirus Premium users only.
+]]
+--[[
+Name = <string> - The name of the UI.
+HidePremium = <bool> - Whether or not the user details shows Premium status or not.
+SaveConfig = <bool> - Toggles the config saving in the UI.
+ConfigFolder = <string> - The name of the folder where the configs are saved.
+IntroEnabled = <bool> - Whether or not to show the intro animation.
+IntroText = <string> - Text to show in the intro animation.
+IntroIcon = <string> - URL to the image you want to use in the intro animation.
+Icon = <string> - URL to the image you want displayed on the window.
+CloseCallback = <function> - Function to execute when the window is closed.
+]]
 print("Blacklisted")
 end
 
