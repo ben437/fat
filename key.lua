7751cec67455949362fd9a8f7b1d4828a91cfaab
ben437 <82418249+ben437@users.noreply.github.com>
2024-05-29 08:01:54 -0700

    Update key.lua

--- key.lua
+++ key.lua
@@ -1,14 +1,6 @@
-local blacklist = {
-		["dieumanase123"] = true,
-	["fat"] = true,
-	["benk20r"] = true
-    
-}
-
-local blacklistfind = blacklist[game.Players.LocalPlayer.Name]
 
 local filename = "niggerv2whitlisted"
-if readfile(filename) == nil then
+
 local key = "niggerv2.nigger"
 local oh = nil
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
@@ -17,7 +9,7 @@ local Tab = Window:MakeTab({
 	Name = "Key",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false,
-    
+
 })
 local Section = Tab:AddSection({
 	Name = "Verify"
@@ -27,24 +19,24 @@ Tab:AddTextbox({
 	Default = "ENTER_KEY",
 	TextDisappear = false,
 	Callback = function(Value)
-  oh = Value
+		oh = Value
 		print(oh)
 	end	  
 })
 Tab:AddButton({
 	Name = "Check Key",
 	Callback = function()
-    if key == oh then
-      	print("Entered right key.")
-          OrionLib:Destroy()
-	loadstring(game:HttpGet(('https://raw.githubusercontent.com/ben437/fat/main/realkey1.lua')))()				
-          loadstring(game:HttpGet('https://raw.githubusercontent.com/ben437/fat/main/yeah.rizz'))()
-          writefile(filename, "sigma")
-          else
-          print("Entered wrong key.")
-          print(oh)
-    end
-  	end    
+		if key == oh then
+			print("Entered right key.")
+			OrionLib:Destroy()
+			loadstring(game:HttpGet(('https://raw.githubusercontent.com/ben437/fat/main/realkey1.lua')))()				
+			loadstring(game:HttpGet('https://raw.githubusercontent.com/ben437/fat/main/yeah.rizz'))()
+			writefile(filename, "sigma")
+		else
+			print("Entered wrong key.")
+			print(oh)
+		end
+	end    
 })
 local Section = Tab:AddSection({
 	Name = "Other"
@@ -56,8 +48,8 @@ Name = <string> - The name of the section.
 Tab:AddButton({
 	Name = "Get key",
 	Callback = function()
-      		setclipboard("https://discord.gg/hcuk5DhUjG")
-  	end    
+		setclipboard("https://discord.gg/hcuk5DhUjG")
+	end    
 })
 
 --[[
@@ -95,27 +87,27 @@ CloseCallback = <function> - Function to execute when the window is closed.
 
 
 
-else
+
 	if blacklistfind == nil then
-print("Your safe, For now....")
-		
- loadstring(game:HttpGet('https://raw.githubusercontent.com/ben437/fat/main/yeah.rizz'))()
-else
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Blacklisted", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
-local Tab = Window:MakeTab({
-	Name = "Blacklisted",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-Tab:AddParagraph("Why were you blacklisted?","You left the discord after using key.")
-Tab:AddButton({
-	Name = "Click to copy the discord server link to be unblacklisted",
-	Callback = function()
-      	setclipboard("https://discord.gg/TzceVeAbf9")
-  	end    
-})
-Tab:AddParagraph("How to be unblacklisted?","Mention the server manager, If no response wait.")
+		print("Your safe, For now....")
+
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/ben437/fat/main/yeah.rizz'))()
+	else
+		local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+		local Window = OrionLib:MakeWindow({Name = "Blacklisted", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+		local Tab = Window:MakeTab({
+			Name = "Blacklisted",
+			Icon = "rbxassetid://4483345998",
+			PremiumOnly = false
+		})
+		Tab:AddParagraph("Why were you blacklisted?","You left the discord after using key.")
+		Tab:AddButton({
+			Name = "Click to copy the discord server link to be unblacklisted",
+			Callback = function()
+				setclipboard("https://discord.gg/TzceVeAbf9")
+			end    
+		})
+		Tab:AddParagraph("How to be unblacklisted?","Mention the server manager, If no response wait.")
 --[[
 Name = <string> - The name of the button.
 Callback = <function> - The function of the button.
@@ -136,8 +128,8 @@ IntroIcon = <string> - URL to the image you want to use in the intro animation.
 Icon = <string> - URL to the image you want displayed on the window.
 CloseCallback = <function> - Function to execute when the window is closed.
 ]]
-print("Blacklisted")
-end
+		print("Blacklisted")
+	end
 
 
-end
+
