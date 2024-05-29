67f3ed3e7246ee8c183159ce9f7fd1d797f1fc18
ben437 <82418249+ben437@users.noreply.github.com>
2024-05-28 22:24:13 -0700

    Update Realkey.lua

--- Realkey.lua
+++ Realkey.lua
@@ -8,37 +8,8 @@ local blacklist = {
 local blacklistfind = blacklist[game.Players.LocalPlayer.Name]
 -- This will print: true
 if blacklistfind == nil then
-	 local userId = game.Players.LocalPlayer.UserId
-local playername = game.Players.LocalPlayer.Name
-local OSTime = os.time();
-local Time = os.date('!*t', OSTime);
-local Avatar = 'https://cdn.discordapp.com/embed/avatars/4.png';
-local Content = 'User executed';
-local Embed = {
-    title = 'cool';
-    color = '99999';
-    footer = { text = game.JobId };
-    author = {
-        name = 'ROBLOX';
-        url = 'https://www.roblox.com/';
-    };
-    fields = {
-        {
-            name = 'Someone used';
-            value = playername;
-        }
-    };
-    timestamp = string.format('%d-%d-%dT%02d:%02d:%02dZ', Time.year, Time.month, Time.day, Time.hour, Time.min, Time.sec);
-};
-(syn and syn.request or http_request) {
-    Url = 'https://discord.com/api/webhooks/1242982544241070172/0QLp70fho_w5Q8a-3wqhOxkSLEEBoGsqB85FbkFx-VD0akxGIETU9Vp80dOyT17JRROo';
-    Method = 'POST';
-    Headers = {
-        ['Content-Type'] = 'application/json';
-    };
-    Body = game:GetService'HttpService':JSONEncode( { content = Content; embeds = { Embed } } );
-};
 	print("Your safe, For now....")
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/ben437/fat/main/gfatdfa131'))()
 else
 
 
